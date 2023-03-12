e56830540110b019483df5e69aa61dde7c026d20
Sangeeth Joseph <you@example.com>
2023-03-12 14:11:06 +0530

    added template functionality

--- src/Components/MailInput.js
+++ src/Components/MailInput.js
@@ -1,64 +1,85 @@
+import { useState, useEffect } from 'react'
 
-import {useState} from 'react'
-// import './App.css';
-const MailInput = () => {
-    const [rawMail,setMail] = useState("")
-    const [response, setResponse] = useState('');
-    const [loading, setLoading] = useState(false);
-    
+const MailInput = ({ mailDescription }) => {
+  const [rawMail, setMail] = useState(mailDescription || '')
+  const [response, setResponse] = useState('')
+  const [loading, setLoading] = useState(false)
 
-    const handleSubmit = async(e) =>{
-        e.preventDefault();
-        const prompt = e.target.mail.value;
-        console.log("prompt",prompt)
-        setLoading(true);
-        // sending request to the cohere API
-        try {
-            const res = await fetch('http://localhost:8000/generate_mail', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({ prompt })
-            });
-            const data = await res.json();
-            console.log(data)
-            setResponse(data);
-            setMail(data.message);
-            setLoading(false);
-          } catch (error) {
-            console.error(error);
-            setLoading(false);
-          }
-      
-
+  useEffect(() => {
+    const selectedTemplate = localStorage.getItem('selectedTemplate');
+    if (selectedTemplate) {
+      setMail(selectedTemplate);
+      localStorage.removeItem('selectedTemplate'); // clear the stored template after using it
     }
+  }, []);
+  
 
-    
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const prompt = e.target.mail.value
+    setLoading(true)
+    // sending request to the cohere API
+    try {
+      const res = await fetch('http://localhost:8000/generate_mail', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ prompt })
+      })
+      const data = await res.json()
+      console.log(data)
+      setResponse(data)
+      setMail(data.message)
+      setLoading(false)
+    } catch (error) {
+      console.error(error)
+      setLoading(false)
+    }
+  }
 
-    return <div>
-        <form onSubmit={handleSubmit}>
-            <p>enter mail desscription to create your professional mail.</p>
-            <br/>
-            <textarea placeholder="example: Create a mail to the hr apply for leave from monday to wednesday." rows="5" cols="30" onChange={(e) => setMail(e.target.value)} name="mail" required></textarea>
-            <br/> <br/>
-            <div  className="livebox">
-            <p>{rawMail}</p>
-            </div>
-            <button className="theme-button"  disabled={loading}>
-              {loading ? (
-                <>
-                  <span className="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span>
-                  Loading...
-                </>
-              ) : (
-                "Submit"
-              )}
-            </button>
-            {loading && <p>Creating the perfect email..</p>}
-        </form>
+  useEffect(() => {
+    if (mailDescription) {
+      setMail(mailDescription)
+    }
+  }, [mailDescription])
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <p>enter mail desscription to create your professional mail.</p>
+        <br />
+        <textarea
+          placeholder="example: Create a mail to the hr apply for leave from monday to wednesday."
+          rows="5"
+          cols="30"
+          onChange={(e) => setMail(e.target.value)}
+          name="mail"
+          value={rawMail}
+          required
+        ></textarea>
+        <br /> <br />
+        <div className="livebox">
+          <p>{rawMail}</p>
+        </div>
+        <button className="theme-button" disabled={loading}>
+          {loading ? (
+            <>
+              <span
+                className="spinner-border spinner-border-sm mr-2"
+                role="status"
+                aria-hidden="true"
+              ></span>
+              Loading...
+            </>
+          ) : (
+            'Submit'
+          )}
+        </button>
+        {loading && <p>Creating the perfect email..</p>}
+      </form>
     </div>
-    }
+  )
+}
 
-
-export default MailInput;
+export default MailInput
--- src/Pages/Templates.js
+++ src/Pages/Templates.js
@@ -1,4 +1,8 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 const Templates = () => {
+    const navigate = useNavigate();
     return <div>
         <br/><br/>
         <h1>Templates</h1>
@@ -6,7 +10,7 @@ const Templates = () => {
         </p>
         <div class="grid-container">
             <div class="grid-cell">
-                <p className="p-with-linebreaks small-text left-text"><strong>Meeting Reminder</strong><br/>
+                <p id="template1"className="p-with-linebreaks small-text left-text"><strong>Meeting Reminder</strong><br/>
                     <small >Subject: Reminder - Meeting Tomorrow at 10 AM<br/>
 
 Dear [Name],<br/>
@@ -20,12 +24,17 @@ Please let me know if you have any questions or concerns before the meeting. I l
 Best regards,<br/>
 [Your Name]</small>
 <br/><br/>
-<button className="theme-button" >
-              Use
-            </button>
-                </p>
+</p>
+<button className="theme-button" onClick={() => {
+    const template = document.getElementById('template1').textContent;
+  localStorage.setItem('selectedTemplate', template);
+  navigate('/'); // Navigate to the home page
+}}>
+  Use
+</button>
+               
                 </div>
-            <div class="grid-cell"><p className="p-with-linebreaks small-text left-text"><strong>Meeting Reminder</strong><br/>
+            <div class="grid-cell"><p id="template2" className="p-with-linebreaks small-text left-text"><strong>Meeting Reminder</strong><br/>
                     <small >Subject: Reminder - Meeting Tomorrow at 10 AM<br/>
 
 Dear [Name],<br/>
@@ -38,12 +47,16 @@ Please let me know if you have any questions or concerns before the meeting. I l
 
 Best regards,<br/>
 [Your Name]</small>
-<br/><br/>
-<button className="theme-button" >
-              Use
-            </button>
-                </p></div>
-            <div class="grid-cell"><p className="p-with-linebreaks small-text left-text"><strong>Meeting Reminder</strong><br/>
+<br/><br/> </p>
+<button className="theme-button" onClick={() => {
+    const template = document.getElementById('template2').textContent;
+  localStorage.setItem('selectedTemplate', template);
+  navigate('/'); // Navigate to the home page
+}}>
+  Use
+</button>
+               </div>
+            <div class="grid-cell"><p id="template3" className="p-with-linebreaks small-text left-text"><strong>Meeting Reminder</strong><br/>
                     <small >Subject: Reminder - Meeting Tomorrow at 10 AM<br/>
 
 Dear [Name],<br/>
@@ -56,17 +69,133 @@ Please let me know if you have any questions or concerns before the meeting. I l
 
 Best regards,<br/>
 [Your Name]</small>
-<br/><br/>
-<button className="theme-button" >
-              Use
-            </button>
-                </p></div>
+<br/><br/></p>
+<button className="theme-button" onClick={() => {
+    const template = document.getElementById('template3').textContent;
+  localStorage.setItem('selectedTemplate', template);
+  navigate('/'); // Navigate to the home page
+}}>
+  Use
+</button>
+                </div>
+
+            
+
+                <div class="grid-cell"><p id="template4" className="p-with-linebreaks small-text left-text"><strong>Job Interview Invitation</strong><br/>
+<small>Subject: Invitation to Job Interview - [Position]<br/><br/>
+
+Dear [Name],<br/><br/>
+
+I am pleased to inform you that your application for the position of [Position] has been successful, and we would like to invite you to an interview at our office.<br/><br/>
+
+The interview will take place on [Date] at [Time] in our conference room located on the [Floor] floor. Please bring a copy of your resume and any other relevant documents.<br/><br/>
+
+If you have any questions about the interview, please do not hesitate to contact us.<br/><br/>
+
+We look forward to meeting you and discussing your application further.<br/><br/>
+
+Best regards,<br/>
+[Your Name]<br/>
+[Company Name]</small><br/><br/>
+</p>
+
+<button className="theme-button" onClick={() => {
+    const template = document.getElementById('template4').textContent;
+  localStorage.setItem('selectedTemplate', template);
+  navigate('/'); // Navigate to the home page
+}}>
+  Use
+</button>
+               </div>
+
+
+
+               <div class="grid-cell"><p id="template5" className="p-with-linebreaks small-text left-text"><strong>Sales Promotion</strong><br/>
+<small>Subject: [Discount] off everything in our store!<br/><br/>
+
+Dear [Name],<br/><br/>
+
+We wanted to let you know about our latest promotion - [Discount] off everything in our store! This is the perfect time to purchase that item you have been eyeing.<br/><br/>
+
+Our store has a wide range of products that you may be interested in, and our knowledgeable staff will be happy to assist you with any questions you may have.<br/><br/>
+
+The promotion is valid until [Date], so don't miss out on this fantastic opportunity.<br/><br/>
+
+Thank you for your continued patronage, and we hope to see you soon!<br/><br/>
+
+Best regards,<br/>
+[Your Name]<br/>
+[Company Name]</small><br/><br/>
+</p>
+
+
+<button className="theme-button" onClick={() => {
+    const template = document.getElementById('template5').textContent;
+  localStorage.setItem('selectedTemplate', template);
+  navigate('/'); // Navigate to the home page
+}}>
+  Use
+</button>
+               </div>
+
+               <div class="grid-cell"><p id="template6" className="p-with-linebreaks small-text left-text"><strong>Change of Business Hours</strong><br/>
+<small>Subject: Important - Change of Business Hours<br/><br/>
+
+Dear [Name],<br/><br/>
+
+We wanted to inform you that starting from [Date], our business hours will change to the following:<br/><br/>
+
+Monday to Friday: [New Hours]<br/>
+Saturday: [New Hours]<br/>
+Sunday: [New Hours]<br/><br/>
+
+We apologize for any inconvenience this may cause, but we hope that these changes will allow us to better serve our customers.<br/><br/>
+
+If you have any questions or concerns, please do not hesitate to contact us.<br/><br/>
+
+Best regards,<br/>
+[Your Name]<br/>
+[Company Name]</small><br/><br/>
+</p>
+
+<button className="theme-button" onClick={() => {
+    const template = document.getElementById('template6').textContent;
+  localStorage.setItem('selectedTemplate', template);
+  navigate('/'); // Navigate to the home page
+}}>
+  Use
+</button>
+               </div>
+
+
+               <div class="grid-cell"><p id="template7" className="p-with-linebreaks small-text left-text"><strong>New Product Launch</strong><br/>
+<small>Subject: Introducing Our Latest Product - [Product Name]<br/><br/>
+
+Dear [Name],<br/><br/>
+
+We are excited to announce the launch of our latest product - [Product Name]! This product is a game-changer and is sure to revolutionize the industry.<br/><br/>
+
+[Product Description]<br/><br/>
+
+We invite you to visit our store and try out our new product. Our knowledgeable staff will be happy to answer any questions you may have.<br/><br/>
+
+Thank you for your continued patronage, and we hope to see you soon!<br/>
+[Your Name]</small>
+<br/><br/> </p>
+<button className="theme-button" onClick={() => {
+    const template = document.getElementById('template7').textContent;
+  localStorage.setItem('selectedTemplate', template);
+  navigate('/'); // Navigate to the home page
+}}>
+  Use
+</button>
+               </div>
         
         </div>
         <style>{`
         .grid-container {
           display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+          grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
           grid-gap: 10px;
         //   border: 1px solid #ffffff;
           padding: 70px;
